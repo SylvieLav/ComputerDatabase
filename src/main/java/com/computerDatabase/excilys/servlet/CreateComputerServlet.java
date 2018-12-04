0119c06cf9e2a284faedfb8b59eb57f4eeb851f8
SylvieLav <sylvie.lav94@gmail.com>
2018-12-04 18:05:30 +0100

    Committed git.ignore

--- src/main/java/com/computerDatabase/excilys/servlet/CreateComputerServlet.java
+++ src/main/java/com/computerDatabase/excilys/servlet/CreateComputerServlet.java
@@ -1,6 +1,7 @@
 package com.computerDatabase.excilys.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,19 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.computerDatabase.excilys.model.Computer;
+import com.computerDatabase.excilys.service.ComputerService;
+
 /**
 * Servlet implementation class Servlet
 */
-@WebServlet("/CreateComputerServlet")
-public class CreateComputerServlet extends HttpServlet {
+@WebServlet("/ListComputerServlet")
+public class ListComputerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static ComputerService computerService = ComputerService.getInstance();
       
-   public CreateComputerServlet() {
+   public ListComputerServlet() {
        super();
    }
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/views/addComputer.jsp").forward(request, response);
+		System.out.println("Je suis dans ListComputerServlet");
+		this.getServletContext().getRequestDispatcher("/WEB-INF/listComputer.jsp").forward(request, response);
+		List<Computer> computers = computerService.listService(1, 10);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
